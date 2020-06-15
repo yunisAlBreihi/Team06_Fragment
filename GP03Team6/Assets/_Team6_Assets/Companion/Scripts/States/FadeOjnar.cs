7e97c07081f3c59f8cc0632d657ec785d855e221
ironm102 <yunis.albreihi@gmail.com>
2020-06-16 01:40:08 +0200

    fixed dialogues in lvl 2 and changed the shader in lvl 2.5

--- GP03Team6/Assets/_Team6_Assets/Companion/Scripts/States/FadeOjnar.cs
+++ GP03Team6/Assets/_Team6_Assets/Companion/Scripts/States/FadeOjnar.cs
@@ -13,10 +13,10 @@ public class FadeOjnar : MonoBehaviour
     {
         myMaterials = gameObject.GetComponentInChildren<SkinnedMeshRenderer>().materials;
 
-        foreach (var item in myMaterials)
-        {
-            originalColors.Add(item.GetColor("_BaseColor"));
-        }
+        //foreach (var item in myMaterials)
+        //{
+        //    originalColors.Add(item.GetColor("_BaseColor"));
+        //}
         StartCoroutine(Fade());
     }
 
@@ -25,17 +25,17 @@ public class FadeOjnar : MonoBehaviour
         float fadeDelta = 0;
         List<Color> endColors = new List<Color>();
 
-        foreach (var item in myMaterials)
-        {
-            endColors.Add(new Color(item.GetColor("_BaseColor").r, item.GetColor("_BaseColor").g, item.GetColor("_BaseColor").b, 0.0f));
-        }
+        //foreach (var item in myMaterials)
+        //{
+        //    endColors.Add(new Color(item.GetColor("_BaseColor").r, item.GetColor("_BaseColor").g, item.GetColor("_BaseColor").b, 0.0f));
+        //}
 
         while (fadeDelta <=1.0f)
         {
-            for (int i = 0; i < myMaterials.Length; i++)
-            {
-                myMaterials[i].SetColor("_BaseColor", Color.Lerp(originalColors[i], endColors[i], fadeDelta));
-            }
+            //for (int i = 0; i < myMaterials.Length; i++)
+            //{
+            //    myMaterials[i].SetColor("_BaseColor", Color.Lerp(originalColors[i], endColors[i], fadeDelta));
+            //}
             fadeDelta += Time.deltaTime / fadeTime;
 
             yield return new WaitForSeconds(1.0f / 30.0f);
